03eb661eb64de76195c0390a87876aa3177f699c
varma1003 <mvvsvarma10@gmail.com>
2021-03-16 00:06:16 +0100

    failed testcase

--- src/test/java/TestMainClass.java
+++ src/test/java/TestMainClass.java
@@ -5,24 +5,22 @@ import org.junit.Test;
 public class TestMainClass {
     private static Main st;
 
-    @BeforeClass
+
     public static void setup() {
         st =new Main();
     }
 
-
-    @Test
     //test by varma
     public void test_circle_area() {
         assertEquals(50,st.circle_area(4));
     }
 
-    @Test
+
     public void test_rectangle_area() {
         assertEquals(25,st.rectangle_area(4,5));
     }
 
-    @Test
+
     public void test_triangle_area() {
         assertEquals(10,st.triangle_area(4,5));
     }
