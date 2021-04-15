38df683f9348283487ceea5d239cd701051593e5
varma1003 <mvvsvarma10@gmail.com>
2021-04-15 21:59:47 +0200

    push

---
+++ src/test/test/TestMainClass.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestMainClass {
+    private static Main st;
+
+    @BeforeClass
+    public static void setup() {
+        st = new Main();
+    }
+
+    //test by varma
+    @Test
+    public void test_circle_area() {
+        assertEquals(50, st.circle_area(4));
+    }
+
+    //New test case but passed case
+    @Test
+    public void test_circle_area1() {
+        assertEquals(28, st.circle_area(3));
+    }
+
+    @Test
+    public void test_rectangle_area() {
+        assertEquals(20, st.rectangle_area(4, 5));
+    }
+
+    @Test
+    public void test_triangle_area() {
+        assertEquals(10, st.triangle_area(4, 5));
+    }
+}
