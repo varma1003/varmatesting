5cd87d6e34219c79d1f359e581f41e743025624f
varma1003 <mvvsvarma10@gmail.com>
2021-04-15 22:57:38 +0200

    observe8fail

--- src/test/test/TestMainClass.java
+++ src/test/test/TestMainClass.java
@@ -10,19 +10,15 @@ public class TestMainClass {
         st = new Main();
     }
 
-    //test by varma
+
+
     @Test
-    public void test_circle_area() {
-        assertEquals(5, st.circle_area(4));
+    public void test_rectangle_area() {
+        assertEquals(20, st.rectangle_area(4, 5));
     }
 
-    //New test case but passed case
     @Test
-    public void test_circle_area1() {
-        assertEquals(28, st.circle_area(3));
+    public void test_triangle_area() {
+        assertEquals(10, st.triangle_area(4, 5));
     }
-
-
-
-
 }
