3e2da6f4bc094027e6ddc1ebf83314e9b9129195
varma1003 <mvvsvarma10@gmail.com>
2021-04-15 22:43:23 +0200

    observe4pass

--- src/test/test/TestMainClass.java
+++ src/test/test/TestMainClass.java
@@ -16,17 +16,5 @@ public class TestMainClass {
         assertEquals(50, st.circle_area(4));
     }
 
-    //New test case but passed case
-    @Test
-    public void test_circle_area1() {
-        assertEquals(28, st.circle_area(3));
-    }
-
-    @Test
-    public void test_rectangle_area() {
-        assertEquals(20, st.rectangle_area(4, 5));
-    }
-
-
 
 }
