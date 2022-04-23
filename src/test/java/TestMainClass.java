33d15c38a08fd778804dc492b50bd4a9951ecdeb
vamsishree <vamsishree06@gmail.com>
2022-04-23 03:36:06 +0200

    Integration tests were written.....

--- src/test/java/TestMainClass.java
+++ src/test/java/TestMainClass.java
@@ -24,7 +24,7 @@ public class TestMainClass {
 
     @Test
     public void test_area(){
-        assertEquals(12, st.circle_area(3));
+        assertEquals(28, st.circle_area(3));
     }
 
     //Integration tests
@@ -34,6 +34,6 @@ public class TestMainClass {
     }
     @Test
     public void integration_test2(){
-        assertEquals(336,st.rectangle_area(st.triangle_area(5,5),st.circle_area(3)));
+        assertEquals(144,st.rectangle_area(st.triangle_area(5,5),st.circle_area(3)));
     }
 }
