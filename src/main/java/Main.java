e8d78b0f4c6be5c8d08267d86888dbd3dbc494a9
vamsishree <vamsishree06@gmail.com>
2022-04-26 23:08:29 +0200

    wrong functionality and right test cases...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,17 @@
 public class Main {
 
     public int circle_area(int r){
-        double c_area = r * r * 3.14;
-        return (int) c_area;
+        return 0;
     }
 
     public int rectangle_area(int h, int l){
         double r_area = h * l;
-        return (int) r_area;
+        return 0;
     }
 
     public int triangle_area(int h, int b){
-        double t_area = (h * b)/2;
-        return (int) t_area;
+        return 0;
     }
 }
+
+//pushing the code with wrong functionality and right test cases
